2e5bb10661c6391c61f6b716a9371d896c0716b4
kevinrioja <kevinrioja216@gmail.com>
2023-10-04 22:34:43 -0400

    Creada funcion para recuperar lo que se escribe en la barra de busqueda

--- src/components/seeker/seeker.jsx
+++ src/components/seeker/seeker.jsx
@@ -1,21 +1,36 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import './seeker.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import swal from "sweetalert";
 
 class Seeker extends Component{
+    
+    
     render(){
+        const handleChange=e=>{
+            console.log("Busqueda: "+e.target.value);
+        }
+        const mostrarAlerta=()=>{
+            swal({
+                title: "Busqueda Finalizada",
+                text: "Parece que perdi la memoria porque no encuentro eso.",
+                icon: "warning"
+            })
+        }
         return(
+            
             <div className="containerInput">
                 <input
                     className="form-control inputBuscar"
                     placeholder="¿Que quieres buscar hoy?"
+                    onChange={handleChange}
                 />
                 <button className="btn btn-filtro">
                     <FontAwesomeIcon icon={faFilter}/>
                 </button>
-                <button className="btn btn-success">
+                <button className="btn btn-success" onClick={()=> mostrarAlerta()}>
                     <FontAwesomeIcon icon={faSearch}/>
                 </button>
             </div>
